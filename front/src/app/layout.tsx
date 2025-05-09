d93104d49e05bb4b230607d014b1b009597ea9f7
unmin <unminaru@gmail.com>
2025-05-09 15:53:21 +0300

    initial offline

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,28 +1,52 @@
-import type { Metadata } from "next";
+'use client'
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "../providers/ReactQueryProvider";
+import { OfflineIndicator } from "@/components/OfflineIndicator";
+import { useEffect } from 'react';
 
 const inter = Inter({
     subsets: ["latin"],
     display: "swap",
 });
 
-export const metadata: Metadata = {
-    title: "Melodify",
-    description: "Your music streaming app",
-};
-
 export default function RootLayout({
     children,
-}: Readonly<{
+  }: {
     children: React.ReactNode;
-}>) {
+  }) {
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            // First, unregister any existing service workers
+            navigator.serviceWorker.getRegistrations().then((registrations) => {
+                registrations.forEach((registration) => {
+                    registration.unregister();
+                });
+            }).then(() => {
+                // Then register the new service worker
+                return navigator.serviceWorker.register('/sw.js');
+            }).then((registration) => {
+                console.log('Service Worker registration successful:', registration.scope);
+            }).catch((error) => {
+                console.error('Service Worker registration failed:', error);
+            });
+        }
+    }, []);
+  
     return (
-        <html lang="en">
-            <body className={`${inter.className} h-full m-0 bg-[#121212]`}>
-                <ReactQueryProvider>{children}</ReactQueryProvider>
+        <html lang="en" className={inter.className}>
+            <head>
+                <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
+            </head>
+            <body>
+           
+                <ReactQueryProvider>
+                <main className="relative min-h-screen">
+                    {children}
+                    <OfflineIndicator />
+                </main>
+                </ReactQueryProvider>
             </body>
         </html>
     );
-}
+  }
